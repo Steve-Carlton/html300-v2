a0de002fb0559c3739950734cbf8bb76cc6b2e1b
stevecarlton <stcarlton@gmail.com>
2021-02-23 14:14:29 -0800

    WIP add twitter icon/links

--- lesson07/assignment/sp1/src/App.vue
+++ lesson07/assignment/sp1/src/App.vue
@@ -5,20 +5,12 @@
 
   <navbar></navbar>
   <main>
-    <!-- <jumbotron v-bind:jumboCopy="jumboCopy" :jumboTagline="jumboTagline" :jumboSubTag="jumboSubTag" :jumboGraphics="jumboGraphics">
-    </jumbotron> -->
-
 <!-- Loop through an array of objects. create a prop to attach array items to. supply a key for vue. -->
     <jumbotron v-for="item in jumbotronArray"
       v-bind:jumboprop="item"
       v-bind:key="item.id">
-      {{jumboTagline}}
+      hihihi
     </jumbotron>
-
-    <todoitem v-for="item in groceryList"
-      v-bind:todo="item"
-      v-bind:key="item.id">{{todoitem}}
-    </todoitem>
   </main>
 </div>
 </template>
@@ -29,35 +21,21 @@
 import BannerTop from './components/BannerTop.vue'
 import navbar from './components/Navbar.vue'
 import jumbotron from './components/Jumbotron.vue'
-import todoitem from './components/todoitem.vue'
+
 
 // ###### VUE.js
 export default {
   el: '#app',
-  components: {BannerTop, navbar, jumbotron, todoitem},//filename of new component
+  components: {BannerTop, navbar, jumbotron},//filename of new component
   data() {
     return {
       brandTitle: "Seattle Playgrounds", //content of prop brandTitle of bannerTop tag
-      // jumboTagline: "Your newest little lifehack.",
-      // jumboSubTag: "Spread the Love!",
-      // // jumboGraphics: [graphics here],
-      // jumboCopy: "Hey there smartypants! Want to help this project? Go on and tweet about it! It's simple, it's easy, and it's a win-win! ;D",
-      // jumboGraphics: "[insert graphics here]",
-
-      groceryList: [//this is an array of objects
-   { id: 4, text: 'Vegetables' },
-   { id: 5, text: 'Cheese' },
-   { id: 6, text: 'Whatever else humans are supposed to eat' }
- ],
-
- jumbotronArray: [//this is an array of objects
-{ id: 0, jumboTagline: "Your newest little lifehack.", jumboSubTag: "Spread the Love!", jumboCopy: "Hey there smartypants! Want to help this project? Go on and tweet about it! It's simple, it's easy, and it's a win-win! ;D"},
-{ id: 1, jumboTagline: "All local playground info in one place.", jumboSubTag: "You're too busy for multi-tabbing.", jumboCopy: "They're our playgrounds afterall—- let's make it easy to get there." },
-{ id: 2, jumboTagline: "On the go from the ground up", jumboSubTag: "This site works across all devices. And does it well.", jumboCopy: "Seattle Playgrounds is the only playground resource thoughfully designed & fully operational across all your devices."}
-]
-
+      jumbotronArray: [//this is an array of objects
+      { id: 0, jumboTagline: "Your newest little lifehack.", jumboSubTag: "Spread the Love!", jumboGraphics: "Tweet #SeattlePGs", jumboCopy: "Hey there smartypants! Want to help this project? Go on and tweet about it! It's a win-win! ;D"},
+      { id: 1, jumboTagline: "All local playground info in one place.", jumboSubTag: "You're too busy for multi-tabbing.", jumboCopy: "They're your playgrounds afterall—- let's make it easy to get there!" },
+      { id: 2, jumboTagline: "On the go from the ground up", jumboSubTag: "This site works across all devices. And does it well.", jumboCopy: "Seattle Playgrounds is the only playground resource thoughfully designed & fully operational across all your devices."}
+      ]
     }
-
   }
 
   // ###### methods: {}
--- lesson07/assignment/sp1/src/components/Jumbotron.vue
+++ lesson07/assignment/sp1/src/components/Jumbotron.vue
@@ -2,13 +2,15 @@
   <section>
     <h2>{{jumboprop.jumboTagline}}</h2>
     <h3 class="lead">{{jumboprop.jumboSubTag}}</h3>
-    <div class="CTAcontainer"> {{jumboprop.jumboGraphics}}</div>
+    <div class="CTAcontainer">
+      <a href="https://www.twitter.com/" target="blank"><i class="fab fa-twitter fa-5x twitterCTA"></i></a>
+      <p><a href="https://www.twitter.com" target="_blank">{{jumboprop.jumboGraphics}}</a></p>
+    </div>
     <p>{{ jumboprop.jumboCopy }}</p>
 
   </section>
   <!-- ##### how to include below block for jumboGraphics? -->
-  <!-- <p><i class="fab fa-twitter fa-5x twitterCTA"></i></p>
-  <a href="https://twitter.com/intent/tweet?button_hashtag=SeattlePGs&ref_src=twsrc%5Etfw" class="twitterIcon twitter-hashtag-button" data-size="large" data-text="Hi! I just discovered Seattle&#39;s best resource for local playgrounds!" data-related="" data-show-count="false">Tweet #SeattlePGs</a> -->
+  <!-- <p><a href="https://www.twitter.com/" target="blank"><i class="fab fa-twitter fa-5x twitterCTA"></i></a></p><a href="https://twitter.com/intent/tweet?button_hashtag=SeattlePGs&ref_src=twsrc%5Etfw" class="twitterIcon twitter-hashtag-button" data-size="large" data-text="Hi! I just discovered Seattle&#39;s best resource for local playgrounds!" data-related="" data-show-count="false" target="blank"> -->
 </template>
 
 <script>
@@ -16,7 +18,7 @@
 export default {
   name: 'jumbotron',//name of file, component, html tag
   props: {
-    //jumboprop is an object. it is an array item in jumbotronArray
+    //jumboprop is an object. it is attached to the array item in jumbotronArray with a v-bind.
     jumboprop: String,
     //my prop name : data type
   }
@@ -43,7 +45,7 @@ export default {
     color: #1DA1F2;
   }
 
-  .CTAcontainer a {
+  .CTAcontainer a.twitterIcon.twitter-hashtag-button {
     padding: .5rem;
     border-radius: 10px;
     color: white;
@@ -51,7 +53,6 @@ export default {
     font-weight: 800;
     margin-bottom: 1rem;
     text-align: center;
-
   }
 
   @media only screen and (max-width: 330px) {
