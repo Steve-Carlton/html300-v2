3c3a8d208ba289ac51d75d176f1ee40bf74b88a7
stevecarlton <stcarlton@gmail.com>
2021-03-09 12:41:28 -0800

    link post component to App.vue

---
+++ lesson08/activity/starter/activity08/src/components/Post.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>confirm Post component</p>
+</template>
+
+<script>
+export default {
+  name: "post",
+}
+</script>
+
+<style></style>
+
+
+<!-- DEV NOTES -->
+<!-- -	 Create a `<post>` component (in its own file) with props for `title`, `excerpt`, `likes`, and `category`. (make sure to import the component to `App.vue`) -->
--- lesson08/activity/starter/activity08/src/views/Home.vue
+++ lesson08/activity/starter/activity08/src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
+    <post></post>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import components
+import post from '../components/Post.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: { post, },
+  data() {
+    return {
+      articleArray: [
+        {id: 0, article: '1'},
+        {id: 1, article: '2'},
+        {id: 2, article: '3'}
+      ]
+    }
   }
 }
 </script>
